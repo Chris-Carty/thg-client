736c59bc196a50053be6def5c062e0de0be42cc2
Chris-Carty <69872982+Chris-Carty@users.noreply.github.com>
2022-12-05 14:37:39 +0000

    init

--- src/rvnu-flow-screens/1_Form.js
+++ src/rvnu-flow-screens/1_Form.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import BoltIcon from '@mui/icons-material/Bolt';
 import FormWrapper from '../rvnu-components/FormWrapper'
 import Subtitle from '../rvnu-components/text/Subtitle'
 import FormButton from '../rvnu-components/Button'
 import 'react-phone-number-input/style.css'
 import ErrorMsg from '../rvnu-components/text/ErrorMsg';
 import HelperText from '../rvnu-components/text/HelperText';
-import PhoneInputCustom from '../rvnu-components/text/PhoneInput'
-import DateOfBirth from '../rvnu-components/DateOfBirth'
+import PayByBankInfo from '../rvnu-components/text/PayByBankInfo'
 import TextFieldUser from '../rvnu-components/text/TextField'
 import clearStorage from '../utils/clearStorage';
 import api from '../utils/api'
-//import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-//import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-//import { Password } from '@mui/icons-material';
+import BankLogos from '../rvnu-assets/bankLogos.svg';
+import RvnuLogo from '../rvnu-assets/RVNU-black.png';
+import RvnuLogo from '../rvnu-assets/RVNU-black.png';
 
 
 
@@ -30,9 +30,8 @@ export default function Form({activeStep, setActiveStep}) {
   // phoneNumber entered bu user will be in E.164 format.
 
   // Required From Fields
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [mobile, setMobile] = useState('');
+  const [amount, setAmount] = useState('');
+  const [payer, setPayer] = useState('');
 
   useEffect(() => {
     // Remove error text
@@ -44,275 +43,107 @@ export default function Form({activeStep, setActiveStep}) {
     // Remove error text
     setError(false)
 
-  }, [firstname, lastname, mobile]);
+  }, [payer, amount]);
 
-    const dobFieldtest = (test) => {
-      switch (test) {
-        case null:
-          return false;
-        default:
-          return true;
-      }
-  }
 
   const delayLoad = () => {
     setLoading(true)
-    setTimeout(handleSubmit, 350)
+    setTimeout(initiatePayment, 350)
   }
 
-  const handleSubmit = () => {
-
-    // DOB
-    const month = localStorage.getItem("dob-month")
-    const day = localStorage.getItem("dob-day")
-    const year = localStorage.getItem("dob-year")
-    const dob = `${day}-${month}-${year}`
-
-    if (
-      dobFieldtest(month) && 
-      dobFieldtest(day) && 
-      dobFieldtest(year) && 
-      firstname && 
-      lastname && 
-      mobile > 3) {
-
-        checkUserExists(mobile, dob)
-        
-      } else {
-        setError(true)
-        setLoading(false)
-      }
-
-}
-
-const checkUserExists = async (mobile, dob) => {
-  // If mobile number has been provided, check if Rvnu user exists
-  const phoneNumber = '+' + mobile
-
-  try{
-    api
-    .get(`/user/getName/` + phoneNumber, {
-      num: phoneNumber,
-    })
-    .then(async (response) => {
-      const result = response.data.data
-
-        if (result.length === 1) {
-          
-            setErrorText("Looks like you've already signed up.")
-            setError(true)
-            setLoading(false)
-            console.log("Error: user already exists")
-
-        } else {
-
-          localStorage.setItem("Firstname", String(firstname))
-          localStorage.setItem("Lastname", String(lastname))
-          localStorage.setItem("Mobile", String(phoneNumber))
-          localStorage.setItem("dateOfBirth", String(dob))
-  
-          // Remove uneccessary vars  
-          localStorage.removeItem('dob-day')
-          localStorage.removeItem('dob-month')
-          localStorage.removeItem('dob-year')
-          setLoading(false)
-          setError(false)
-
-          // Uncomment for production
-          //sendOtp(phoneNumber)
-          // Comment for production
-          setActiveStep(activeStep + 1)
-
-        }
-    })
-    .catch((error) => {
-      console.log(error)
-      setLoading(false)
-    })
-  } catch {
-    console.log("Request Error")
-    setLoading(false) 
-  }
-}
-
-const sendOtp = async (phoneNumber) => {
-
-  // If mobile number has been provided, request OTP code
-  try{
-    api
-    .post(`/verify/sendOtp/` + phoneNumber, {
-      phoneNumber: phoneNumber
-    })
-    .then(async (response) => {
-      setLoading(false)
-      setActiveStep(activeStep + 1)
-    })
-    .catch((error) => {
-      console.log(error)
-      setLoading(false)
-      setError(true)
-    })
-  } catch {
-    console.log("Error sending OTP text") 
-    setLoading(false)
+  const initiatePayment = async () => {
+    setLoading(true)
   }
 
-}
-/*
-// FOR WHEN PASSWORDS ARE ENABLED
 
-  const handleSubmit = () => {
-    // your submit logic
-    console.log(values)
-    // Check email and phone number do not exisit
-}
-
-const handleChange = (prop) => (event) => {
-  setValues({ ...values, [prop]: event.target.value });
-};
-
-
-useEffect(() => {
-       // custom validation rule will have name 'isPasswordMatch'
-       ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
-        if (value !== values.password) {
-            return false;
-        }
-        return true;
-    });
-}, [values.password]);
-
-*/
 
   return (
 
     <FormWrapper>
-      <Subtitle subtitleText={"Create your account"} >
-          <AccountCircleIcon />
+      <Subtitle subtitleText={"RVNU Pay - UAT"} >
+          <AccountBalanceIcon />
       </Subtitle>
-            {/* FIRST NAME */}
-            <HelperText text={`Name`} />
-              <TextFieldUser
-                  required
-                  autoFocus
-                  id="firstName"
-                  placeholder="Firstname" 
-                  autoComplete="given-name"
-                  value={firstname}
-                  onChange={e => setFirstname(e.target.value)}
-              />
-              <TextFieldUser
-                  required
-                  placeholder="Lastname"
-                  autoComplete="family-name"
-                  value={lastname}
-                  onChange={e => setLastname(e.target.value)}
-              />
-            {/* 
-            <TextValidator
-                fullWidth
-                autoFocus
-                id="firstName"
-                label="Firstname" 
-                placeholder="First Name" 
-                autoComplete="given-name"
-                value={values.firstname}
-                onChange={handleChange('firstname')}
-                validators={['required']}
-                errorMessages={['What’s your firstname?']}
+            <HelperText text={`Welcome to the RVNU user acceptance testing (UAT) space.`} />
+            <HelperText text={`This site is primarily built for RVNU ambassadors to evaluate the RVNU payment experience and provide feedback.`} />
+            <HelperText text={``} />
+            <HelperText text={`Payer name`} />
+            <TextFieldUser
+                    required
+                    id="firstName"
+                    placeholder="Your name" 
+                    autoComplete="given-name"
+                    value={payer}
+                    onChange={e => setPayer(e.target.value)}
             />
-            {/* LAST NAME 
-            <TextValidator
-                fullWidth
-                label="Lastname"  
-                placeholder="Last Name"
-                autoComplete="family-name"
-                value={values.lastname}
-                onChange={handleChange('lastname')}
-                validators={['required']}
-                errorMessages={['What’s your lastname?']}
-            />
-            {/* EMAIL 
-            <TextValidator
-                fullWidth
-                id="email"
-                name="email"
-                autoComplete="email"
-                label="Email"
-                value={values.email}
-                onChange={handleChange('email')}
-                validators={['required', 'isEmail']}
-                errorMessages={['Please enter a valid email.', 'Please enter a valid email.']}
+            <HelperText text={`Payment amount`} />
+            <TextFieldUser
+                  required
+                  placeholder="Amount yout wish to pay" 
+                  value={amount}
+                  onChange={e => setAmount(e.target.value)}
             />
 
-            {/* PASSWORD 
-            <TextValidator
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                autoComplete="new-password"
-                value={values.password}
-                onChange={handleChange('password')}
-                validators={['required', 'matchRegexp:^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$']}
-                errorMessages={['this field is required', 'password is not valid']}
-            />
-             {/* CONFIRM PASSWORD 
-             <TextValidator
-                fullWidth
-                label="Confirm Password"
-                name="repeatPassword"
-                type="password"
-                autoComplete="new-password"
-                value={values.confirmPassword}
-                onChange={handleChange('confirmPassword')}
-                validators={['required', 'isPasswordMatch']}
-                errorMessages={['this field is required', 'password mismatch']}
-            />
-             */}
-             <HelperText text={`Phone`} />
-             <PhoneInputCustom 
-              required
-              error={error}
-              value={mobile}
-              onChange={num => setMobile(num)}
-            />
-            <DateOfBirthWrap>
-              <HelperText 
-                text={`Date of birth`} 
-              />
-              <TextWrap>
-                <Text>
-                  This will not be shown publicly. Confirm your own age.
-                </Text> 
-              </TextWrap>
-              <DateOfBirth />
-            </DateOfBirthWrap>
+            <PayByBankWrap>
+              <InstantBankTextWrap>
+                <HeadlineWrap>
+                  <BoltIcon />
+                  <img src={RvnuLogo}
+                      alt="RVNU Logo" 
+                      height="25"
+                  />
+                </HeadlineWrap>
+                <img src={BankLogos}
+                      alt="RVNU Logo" 
+                      height="25"
+                  />
+              </InstantBankTextWrap>
+              <EducationWrapper>
+                <PayByBankInfo infoText={'Pay instantly via online bank transfer directly from your current account.'} />
+                <PayByBankInfo infoText={'Hassle-free. Connect to your bank without entering data manually'} />
+                <PayByBankInfo infoText={'We accept all major UK banks'} />
+              </EducationWrapper>
+            </PayByBankWrap>
             <FormButton
-              fullWidth
               type="submit"
               loading={loading}
               isButtonDisabled={isButtonDisabled}
-              disabledButtonText={"Next"}
-              buttonText={"Next"}
+              disabledButtonText={"Pay with RVNU"}
+              buttonText={"Pay with RVNU"}
               onClick={() => delayLoad()}
             >
             </FormButton>
-            {/*
-       */}
       { error ? <ErrorMsg errorText={errorText} /> : <p></p> }
     </FormWrapper>
   )
 }
 
+// Styled comononets
+const PayByBankWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1.5px solid #3d984a;
+  border-radius: 10px;
+  padding: 10px;
+`
 
-// Styled components
-const DateOfBirthWrap = styled.section`
-  margin-top: 10px;
+const InstantBankTextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  justify-content: space-between;
 `
-const TextWrap = styled.section`
-  margin-bottom: 25px;
+
+const HeadlineWrap = styled.div`
+  display: flex;
+  align-items: center;
 `
-const Text = styled.section`
-  font-size: 14px;
+
+const Text = styled.p`
+`
+
+const EducationWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  border: 0.5px solid;
+  border-radius: 6px;
 `
